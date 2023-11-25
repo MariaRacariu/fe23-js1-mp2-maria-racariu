7e853b586186e7b9b77e71f199af577c8dfd44a9
Maria Racariu <90383391+MariaRacariu@users.noreply.github.com>
2023-11-25 22:26:26 +0100

    fixed game
    Fixed game to work but no UI yet, created simple console log to view data outputted and alerts until I add UI for user. TO_DO next would be to add some UI for user and show clearly who wins and loses and then also add the 3 rounds

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const buttonPaper = document.querySelector('#buttonPaper');
 const buttonRock = document.querySelector('#buttonRock');
+const buttonPaper = document.querySelector('#buttonPaper');
 const buttonScissors = document.querySelector('#buttonScissor');
+
 const buttonDeleteUser = document.querySelector('#deleteUser');
 
 // Create heading for the username
@@ -22,93 +23,63 @@ function deleteUserName(){
     localStorage.removeItem("userNameStorage");
     location.reload();
 }
+// Add event listener on all buttons and event to return random number
+const gameOptions = ["Rock", "Paper", "Scissors"];
 
+buttonRock.addEventListener("click", (event)=>{
+    let randomPCInput = gameOptions[Math.floor(Math.random()*3)];
+    console.log(randomPCInput);
 
-buttonPaper.addEventListener("click", (event) =>{
-    const mySpecialNumber = 0;
-    let randomNumber = Math.floor(Math.random()*3);
-    myTestFunction(mySpecialNumber, randomNumber);
-}); //Paper 0
-// buttonRock.addEventListener("click", paperCheck); //Rock 1
-// buttonScissors.addEventListener("click", paperCheck); //Scissor 2
+    const userInput = "Rock";
+    console.log(userInput);
 
-function myTestFunction(mySpecialNumber, randomNumber){
-    if(mySpecialNumber === randomNumber){
-        console.log("It's a DRAW");
-    }else if(mySpecialNumber > randomNumber){
-        console.log("special number is higher");
-    }else if(mySpecialNumber < randomNumber){
-        console.log("special number is lower");
-    }else{
-        console.log("Nothing");
+    compareInputs(randomPCInput, userInput);
+});
+buttonPaper.addEventListener("click", (event)=>{
+    let randomPCInput = gameOptions[Math.floor(Math.random()*3)];
+    console.log(randomPCInput);
+
+    const userInput = "Paper";
+    console.log(userInput);
+
+    compareInputs(randomPCInput, userInput);
+});
+buttonScissors.addEventListener("click", (event)=>{
+    let randomPCInput = gameOptions[Math.floor(Math.random()*3)];
+    console.log(randomPCInput);
+
+    const userInput = "Scissors";
+    console.log(userInput);
+
+    compareInputs(randomPCInput, userInput);
+});
+
+function compareInputs(randomPCInput, userInput){
+    const currentGame = `${randomPCInput} VS ${userInput}`;
+    // Check Tie first
+    if(randomPCInput === userInput){
+        alert(`${currentGame} IT'S A TIE`);
+        return;
+    }
+    
+    // Check Rock, Paper and Scissors
+    if(userInput === "Rock"){
+        if(randomPCInput === "Scissors"){
+            alert(`${currentGame} You win`);
+        }else{
+            alert(`${currentGame} You lose`);
+        }
+    }else if(userInput === "Paper"){
+        if(randomPCInput === "Rock"){
+            alert(`${currentGame} You win`);
+        }else{
+            alert(`${currentGame} You lose`);
+        }
+    }else if(userInput === "Scissors"){
+        if(randomPCInput === "Paper"){
+            alert(`${currentGame} You win`);
+        }else{
+            alert(`${currentGame} You lose`);
+        }
     }
 }
-
-
-// function paperCheck(){
-//     function computerNumber(max){
-//         return Math.floor(Math.random() * max);
-//     }
-//     const maxNumber = computerNumber(3);
-// 
-//     switch(maxNumber){
-//         case 0:
-//             console.log("Draw");
-//             break;
-//         case 1:
-//             console.log("You WIN!");
-//             break;
-//         case 2:
-//             console.log("You lose!");
-//             break;
-//         default:
-//             break;
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ---Plan---
-// I could make it so that when you click on a button it gives me it's value
-// Then if the value is the same do nothing
-// If it's a win and then if it's a lose
-
-// ---Info---
-// userInput(Paper)     == Paper        NULL    DRAW
-// userInput(Rock)      == Paper        FALSE   User Lose
-// userInput(Scissors)  == Paper        TRUE    User Win
-
-// userInput(Paper)     == Rock         TRUE    User Win
-// userInput(Rock)      == Rock         NULL    DRAW
-// userInput(Scissors)  == Rock         FALSE   User Lose
-
-// userInput(Paper)     == Scissors     TRUE    User Win
-// userInput(Rock)      == Scissors     FALSE   User Lose
-// userInput(Scissors)  == Scissors     NULL    DRAW
-
-// ---Assignment Information Requirements --
-// ◉ Allow user to use UserName and store in browser with UserName 
-// and how many wins against the computer also allow User to Delete 
-// data from browser cache.
-// ◉ Create buttons for Paper Rock Scissors
-// ◉ Create computer random choice which will be stored
-// ◉ Show the computers choice and user choice on screen
-// ◉ With every round show clearly who won
-// ◉ Who wins will receive a point
-// ◉ The points will be shown on the screen
-// ◉ First to 3 points will win
-// ◉ Show the winner clearly
-// ◉ When the game is over allow user to restart
